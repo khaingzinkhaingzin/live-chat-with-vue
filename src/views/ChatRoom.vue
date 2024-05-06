30a5bdbaa8e7bccfb84a401113438fe962a144ef
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-05-06 22:38:06 +0630

    Always watching ref variable using watch function of vue

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -6,8 +6,22 @@
 
 <script>
 import Navbar from "../components/Navbar";
+import getUser from "../composable/getUser"
+import { useRouter } from 'vue-router'
+import { watch } from 'vue'
 export default {
 	components: { Navbar },
+	setup() {
+		let { user } = getUser();
+		let router = useRouter();
+
+		watch(user, () => {
+			if (!user.value) {
+				// redirect welcome 
+				router.push("/");
+			}
+		})
+	}
 };
 </script>
 
